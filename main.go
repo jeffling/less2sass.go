4d5073873e1192b8a841d79fc47d3c31c6ad3096
Jeffrey Ling <jeff@bench.co>
2015-02-09 17:26:55 -0800

    gofmt

--- main.go
+++ main.go
@@ -1,74 +1,73 @@
 package main
 
 import (
-  "os"
-  "log"
-  "io/ioutil"
-  "path/filepath"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 // array of regex to execute in order
 // {{re, replace string}}
 var lessToSassReplacePairs ReplacePairs = ReplacePairs{
-  []ReplacePair {
-    ReplacePair{`@(?!import|media|keyframes|-)`, `$`},
-    ReplacePair{`\.([\w\-]*)\s*\((.*)\)\s*\{`, `@mixin \1\(\2\)\n{`},
-    ReplacePair{`\.([\w\-]*\(.*\)\s*;)`, `@include \1`},
-    ReplacePair{`~"(.*)"`, `#{"\1"}`},
-    ReplacePair{`spin`, `adjust-hue`},
-  },
+	[]ReplacePair{
+		ReplacePair{`@(?!import|media|keyframes|-)`, `$`},
+		ReplacePair{`\.([\w\-]*)\s*\((.*)\)\s*\{`, `@mixin \1\(\2\)\n{`},
+		ReplacePair{`\.([\w\-]*\(.*\)\s*;)`, `@include \1`},
+		ReplacePair{`~"(.*)"`, `#{"\1"}`},
+		ReplacePair{`spin`, `adjust-hue`},
+	},
 }
 
 func transformLessToSass(content []byte) []byte {
-  return Replacer(lessToSassReplacePairs).Replace(content)
+	return Replacer(lessToSassReplacePairs).Replace(content)
 }
 
 func parseSrc(path string, info os.FileInfo, err error) error {
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  if !info.IsDir() && filepath.Ext(path) == ".less" {
-    content, err := ioutil.ReadFile(path)
-    if err != nil {
-      log.Println("There was an error reading file", path)
-      return err
-    }
+	if !info.IsDir() && filepath.Ext(path) == ".less" {
+		content, err := ioutil.ReadFile(path)
+		if err != nil {
+			log.Println("There was an error reading file", path)
+			return err
+		}
 
-    // write file into destination directory
-    destPath := os.Args[len(os.Args) - 1]
-    addSuffixIfMissing(&destPath, "/")
-    
-    newFilePath := destPath + replaceExt(path, ".scss")
+		// write file into destination directory
+		destPath := os.Args[len(os.Args)-1]
+		addSuffixIfMissing(&destPath, "/")
 
-    err = ioutil.WriteFile(newFilePath, transformLessToSass(content), info.Mode())
+		newFilePath := destPath + replaceExt(path, ".scss")
 
-    if err != nil {
-      log.Println("there was an error writing to", newFilePath)
-      return err
-    }
-  }
-  return err
+		err = ioutil.WriteFile(newFilePath, transformLessToSass(content), info.Mode())
+
+		if err != nil {
+			log.Println("there was an error writing to", newFilePath)
+			return err
+		}
+	}
+	return err
 }
 
 func main() {
-  lastArgIndex := IntMax(len(os.Args) - 1, 1)
+	lastArgIndex := IntMax(len(os.Args)-1, 1)
 
-  if lastArgIndex < 2 {
-    log.Fatal("USAGE: less2sass <srcFile or srcDirectory> ... <destDirectory>")
-  }
-  
-  // check if the last argument is correct
-  if _, err := dirExists(os.Args[lastArgIndex]); err != nil {
-    log.Fatal("The last argument should be the destination directory")
-  }
+	if lastArgIndex < 2 {
+		log.Fatal("USAGE: less2sass <srcFile or srcDirectory> ... <destDirectory>")
+	}
 
-  // walk through the source files/directories
-  for _, filePath := range os.Args[1:lastArgIndex] {
-    if err := filepath.Walk(filePath, parseSrc); err != nil {
-      log.Println("Could not walk through source directory", filePath)
-      log.Println(err)
-    }
-  }
+	// check if the last argument is correct
+	if _, err := dirExists(os.Args[lastArgIndex]); err != nil {
+		log.Fatal("The last argument should be the destination directory")
+	}
+
+	// walk through the source files/directories
+	for _, filePath := range os.Args[1:lastArgIndex] {
+		if err := filepath.Walk(filePath, parseSrc); err != nil {
+			log.Println("Could not walk through source directory", filePath)
+			log.Println(err)
+		}
+	}
 }
-
--- replacer.go
+++ replacer.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-  "github.com/glenn-brown/golang-pkg-pcre/src/pkg/pcre"
+	"github.com/glenn-brown/golang-pkg-pcre/src/pkg/pcre"
 )
 
 type Replacer interface {
-  Replace(content []byte) []byte
+	Replace(content []byte) []byte
 }
 
 func (r ReplacePair) Replace(content []byte) []byte {
-  re := pcre.MustCompile(r.matchBy, 0)
-  return re.ReplaceAll(content, []byte(r.replaceWith), 0)
+	re := pcre.MustCompile(r.matchBy, 0)
+	return re.ReplaceAll(content, []byte(r.replaceWith), 0)
 }
 
 func (replacePairArr ReplacePairs) Replace(content []byte) []byte {
-  newContent := content
-  var r Replacer
-  for _, replacePair := range replacePairArr.replacePairs {
-    r = replacePair
-    newContent = r.Replace(newContent)
-  }
-  return newContent
+	newContent := content
+	var r Replacer
+	for _, replacePair := range replacePairArr.replacePairs {
+		r = replacePair
+		newContent = r.Replace(newContent)
+	}
+	return newContent
 }
 
-type ReplacePair struct { 
-  matchBy, replaceWith string
+type ReplacePair struct {
+	matchBy, replaceWith string
 }
 
-type ReplacePairs struct { 
-  replacePairs []ReplacePair
+type ReplacePairs struct {
+	replacePairs []ReplacePair
 }
